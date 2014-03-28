10b0ef300031acc841ea9491bbd10a7b153388e6
Rafael Fiuza <rafael.fiuza@helabs.com.br>
2014-03-28 14:46:42 -0300

    static content migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140328171905) do
+
+  create_table "static_content_contents", force: true do |t|
+    t.string   "slug"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
