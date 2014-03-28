9b963bc4ef459ca379fed1b59ec21930fef3a339
Rafael Fiuza <rafael.fiuza@helabs.com.br>
2014-03-28 14:02:16 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PStatic::Application.config.secret_key_base = '6e85fb2b3650da7098e14021787aa09326b68bc09993e4bf9843f3a933b851d45a2f9ab0a037839de637d505a1333528173502931a6a3553ffde30dd723b4e7d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PStatic::Application.config.session_store :cookie_store, key: '_p_static_session'
