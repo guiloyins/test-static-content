eceab3cf24ccf166d170f9d050ba13f7563937eb
Rafael Fiuza <rafael.fiuza@helabs.com.br>
2014-03-28 14:05:53 -0300

    Replace secret token with environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PStatic::Application.config.secret_key_base = '6e85fb2b3650da7098e14021787aa09326b68bc09993e4bf9843f3a933b851d45a2f9ab0a037839de637d505a1333528173502931a6a3553ffde30dd723b4e7d'
+PStatic::Application.config.secret_key_base = ENV['SECRET_TOKEN']
